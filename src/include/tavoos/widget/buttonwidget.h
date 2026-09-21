3610fefe2bbc6e1aabee82daab26f64e1ae8e997
AmirHosseinCH <amirhosseinchoghaei@gmail.com>
2026-09-21 15:48:19 +0330

    Add text, font and text colors to ButtonWidget
    ButtonWidget gets a default label with text, a bindable Font, and
    textColor and disabledTextColor. The button resizes when the label changes.

--- src/include/tavoos/widget/buttonwidget.h
+++ src/include/tavoos/widget/buttonwidget.h
@@ -3,14 +3,42 @@
 #include <tavoos/animation/animatedstate.h>
 #include <tavoos/export.hpp>
 #include <tavoos/reactive/reactive.h>
+#include <tavoos/text/font.h>
 #include <tavoos/types.h>
 #include <tavoos/widget/buttonbase.h>
+
+#include <string>
 
 namespace Tavoos {
 
 class TAVOOS_EXPORT ButtonWidget : public ButtonBase {
 public:
     ButtonWidget(Object* parent);
+
+    decltype(auto) text(this auto&& self, PropertyArg<std::string> text) {
+        self.m_text.set(text);
+        return std::forward<decltype(self)>(self);
+    }
+
+    decltype(auto) font(this auto&& self, const Font& font) {
+        self.m_font.set(font);
+        return std::forward<decltype(self)>(self);
+    }
+
+    decltype(auto) font(this auto&& self, State<Font>& font) {
+        self.m_font.set(font);
+        return std::forward<decltype(self)>(self);
+    }
+
+    decltype(auto) textColor(this auto&& self, PropertyArg<Paint> color) {
+        color.applyTo(self.m_textColor);
+        return std::forward<decltype(self)>(self);
+    }
+
+    decltype(auto) disabledTextColor(this auto&& self, PropertyArg<Paint> color) {
+        color.applyTo(self.m_disabledTextColor);
+        return std::forward<decltype(self)>(self);
+    }
 
     decltype(auto) idleColor(this auto&& self, PropertyArg<Paint> color) {
         color.applyTo(self.m_idleColor);
@@ -32,18 +60,27 @@ public:
         return std::forward<decltype(self)>(self);
     }
 
+    std::string text() const { return m_text; }
+    Font font() const { return m_font; }
     float transition() const { return m_transition; }
 
     State<Paint>& colorState() { return m_color; }
 
 private:
     void updateColor(bool animate);
+    void updateTextColor(bool animate);
 
     Property<Paint> m_idleColor{Paint{Color::rgba(60, 130, 255)}};
     Property<Paint> m_hoverColor{Paint{Color::rgba(84, 148, 255)}};
     Property<Paint> m_disabledColor{Paint{Color::rgba(228, 229, 235)}};
     Property<float> m_transition{0.12f};
     AnimatedState<Paint> m_color;
+
+    BindableState<std::string> m_text;
+    BindableState<Font> m_font;
+    Property<Paint> m_textColor{Paint{Color::White}};
+    Property<Paint> m_disabledTextColor{Paint{Color::rgba(160, 163, 175)}};
+    AnimatedState<Paint> m_textColorOut;
 };
 
 }
--- src/widget/buttonwidget.cpp
+++ src/widget/buttonwidget.cpp
@@ -4,19 +4,37 @@ namespace Tavoos {
 
 ButtonWidget::ButtonWidget(Object* parent) : ButtonBase{parent} {
     m_color.set(m_idleColor.get());
+    m_textColorOut.set(m_textColor.get());
 
     m_idleColor.onChange([this](const Paint&) { updateColor(false); });
     m_hoverColor.onChange([this](const Paint&) { updateColor(false); });
     m_disabledColor.onChange([this](const Paint&) { updateColor(false); });
     hoveredState().onChange([this](const bool&) { updateColor(true); });
-    enabledState().onChange([this](const bool&) { updateColor(true); });
+    enabledState().onChange([this](const bool&) {
+        updateColor(true);
+        updateTextColor(true);
+    });
+
+    m_textColor.onChange([this](const Paint&) { updateTextColor(false); });
+    m_disabledTextColor.onChange([this](const Paint&) { updateTextColor(false); });
+    m_text.onChange([this](const std::string&) { requestRelayout(); });
+    m_font.onChange([this](const Font&) { requestRelayout(); });
 
     background([this](RectangleWidget& rect) { rect.radius(8).color(m_color); });
+    content([this](TextWidget& label) {
+        label.text(m_text.state()).font(m_font.state()).color(m_textColorOut)
+            .marginLeft(16).marginRight(16).marginTop(8).marginBottom(8);
+    });
 }
 
 void ButtonWidget::updateColor(bool animate) {
     const Paint& target = !enabled() ? m_disabledColor.get() : hovered() ? m_hoverColor.get() : m_idleColor.get();
     m_color.animateTo(target, animate ? m_transition.get() : 0.0f);
+}
+
+void ButtonWidget::updateTextColor(bool animate) {
+    const Paint& target = enabled() ? m_textColor.get() : m_disabledTextColor.get();
+    m_textColorOut.animateTo(target, animate ? m_transition.get() : 0.0f);
 }
 
 }
